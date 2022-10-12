9b31d9f3f4ec9bfdd77bc4f46395940f58159d5a
Henry Cortez <henry.cortez@airlinemro.parts>
2022-10-12 17:58:30 -0600

    ✅ Add Delete task by id

--- api/tasks/services/tasks.services_test.go
+++ api/tasks/services/tasks.services_test.go
@@ -119,3 +119,69 @@ func TestCreateTask(t *testing.T) {
 		})
 	}
 }
+
+func TestDeleteTaskById(t *testing.T) {
+	testCases := []struct {
+		Name          string
+		Result        models.Task
+		TaskId        string
+		UserId        string
+		ExpectedError error
+	}{
+		{
+			Name:          "Should throw an error if user doesn't exist",
+			Result:        models.Task{},
+			TaskId:        "4",
+			UserId:        "4",
+			ExpectedError: fiber.NewError(fiber.StatusBadRequest, "User not found"),
+		},
+
+		{
+			Name:          "Should throw an error if user is not the task owner",
+			Result:        models.Task{},
+			TaskId:        "5",
+			UserId:        "5",
+			ExpectedError: fiber.NewError(fiber.StatusForbidden, "You cannot delete this task"),
+		},
+
+		{
+			Name:          "Should throw an error if task cannot be deleted",
+			Result:        models.Task{},
+			TaskId:        "6",
+			UserId:        "6",
+			ExpectedError: fiber.NewError(fiber.StatusForbidden, "You cannot delete this task"),
+		},
+
+		{
+			Name:          "Should delete the task",
+			Result:        models.Task{},
+			TaskId:        "7",
+			UserId:        "7",
+			ExpectedError: nil,
+		},
+	}
+
+	repo.On("FindUserById", "4").Return(models.User{}, errors.New("User not found"))
+
+	repo.On("FindUserById", "5").Return(models.User{ID: 5}, nil)
+	repo.On("FindById", "5", cast.ToUint(5)).Return(models.Task{ID: 0}, errors.New("Task doesn't exist"))
+
+	repo.On("FindUserById", "6").Return(models.User{ID: 6}, nil)
+	repo.On("FindById", "6", cast.ToUint(6)).Return(models.Task{ID: 6}, nil)
+	repo.On("DeleteById", "6").Return(errors.New("Cannot delete the task"))
+
+	repo.On("FindUserById", "7").Return(models.User{ID: 7}, nil)
+	repo.On("FindById", "7", cast.ToUint(7)).Return(models.Task{ID: 7}, nil)
+	repo.On("DeleteById", "7").Return(nil)
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			repo.Mock.Test(t)
+			t.Parallel()
+
+			defer func() { recover() }()
+			taskServicesMock.DeleteTaskById(tc.TaskId, tc.UserId)
+		})
+	}
+}
